062ae288d77db83b4d4726291847baa1706e4242
Tim Evteev <timofey@Tims-MacBook-Air.local>
2025-05-28 23:50:20 +0400

    fix: исправления по ESLint

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  rules: {
+    'no-console': 'off',
+    'import/extensions': 'off',
+  },
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+};
--- Makefile
+++ Makefile
@@ -1,9 +1,2 @@
-install:
-	npm ci
-
-brain-games:
-	node bin/brain-games.js
-
-
-publish:
-	npm publish --dry-run
+lint:
+	 npx eslint .
