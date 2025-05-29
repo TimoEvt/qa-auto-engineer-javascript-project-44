ec65b16bcf43cc86b9f96fe3429d0f27b8e3bee6
Tim Evteev <timofey@Tims-MacBook-Air.local>
2025-05-29 18:26:47 +0400

    Добавлена общая логика запуска игр с 3 раундами

---
+++ brain-games/src/index.js
@@ -0,0 +1,26 @@
+import readlineSync from 'readline-sync';
+
+const maxRounds = 3;
+
+export default function runGame(game) {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+
+  for (let round = 0; round < maxRounds; round++) {
+    const { question, correctAnswer } = game.generateRound();
+
+    console.log('Question: ' + question);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (userAnswer !== correctAnswer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+
+    console.log('Correct!');
+  }
+
+  console.log(`Congratulations, ${userName}!`);
+}
