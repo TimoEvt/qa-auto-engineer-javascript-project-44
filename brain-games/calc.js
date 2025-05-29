527dbe0bfd4372fcc69f6adb988eb0a702b4e84e
Tim Evteev <timofey@Tims-MacBook-Air.local>
2025-05-29 16:38:33 +0400

    Добавлена секция bin с командами brain-calc и brain-even

---
+++ brain-games/calc.js
@@ -0,0 +1,35 @@
+const runGameEngine = require('./engine');
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generateCalcRound() {
+  const operations = ['+', '-', '*'];
+  const num1 = getRandomNumber(1, 50);
+  const num2 = getRandomNumber(1, 50);
+  const operator = operations[Math.floor(Math.random() * operations.length)];
+
+  let result;
+  switch (operator) {
+    case '+':
+      result = num1 + num2;
+      break;
+    case '-':
+      result = num1 - num2;
+      break;
+    case '*':
+      result = num1 * num2;
+      break;
+  }
+
+  const question = `${num1} ${operator} ${num2}`;
+  return [question, result];
+}
+
+function main() {
+  const rule = 'What is the result of the expression?';
+  runGameEngine(generateCalcRound, rule);
+}
+
+main();
