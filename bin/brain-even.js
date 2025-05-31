0d1e66913661814329b0dd5a06650fb0a9e83bc2
TimoEvt <t.evteev@school1387.ru>
2025-06-01 01:28:32 +0400

    brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-import runGame from '../src/index.js';
-import { generateRound, gameDescription } from '../src/games/even.js';
-
-runGame(generateRound, gameDescription);
+import runGame from '../src/index.js'
+import { generateRound, gameDescription } from '../src/games/even.js'
+runGame(generateRound, gameDescription)
