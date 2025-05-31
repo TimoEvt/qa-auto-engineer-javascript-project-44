d05d17db435b2d443d3e18a897e326063379f76f
TimoEvt <t.evteev@school1387.ru>
2025-06-01 01:29:09 +0400

    brain-progression.js

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
-import runGame from '../src/index.js';
-import generateRound from '../src/games/brain-progression.js';
-
-const rule = 'What number is missing in the progression?';
-
-runGame(generateRound, rule);
+import runGame from '../src/index.js'
+import generateRound from '../src/games/brain-progression.js'
+const rule = 'What number is missing in the progression?'
+runGame(generateRound, rule)
