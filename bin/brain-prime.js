f92568c8955ae4901a141de7bf934416928baf50
TimoEvt <t.evteev@school1387.ru>
2025-06-01 01:29:44 +0400

    brain-prime.js

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
-import runGame from '../src/index.js';
-import generateRound from '../src/games/brain-prime.js';
-
-const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-
-runGame(generateRound, rule);
+import runGame from '../src/index.js'
+import generateRound from '../src/games/brain-prime.js'
+const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+runGame(generateRound, rule)
