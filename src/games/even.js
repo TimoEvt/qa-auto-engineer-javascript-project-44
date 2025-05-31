e293d1979d8d2d6a91eb63b0fab4bbd75e0f669e
TimoEvt <t.evteev@school1387.ru>
2025-06-01 01:12:21 +0400

    even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,14 +1,10 @@
-const getRandomNumber = (max = 100) => Math.floor(Math.random() * max);
-
-const isEven = (num) => num % 2 === 0;
-
-const gameDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
-
+const getRandomNumber = (max = 100) => Math.floor(Math.random() * max)
+const isEven = (num) => num % 2 === 0
+const gameDescription = 'Answer "yes" if the number is even, otherwise answer "no".'
 const generateRound = () => {
-  const number = getRandomNumber();
-  const question = number.toString();
-  const correctAnswer = isEven(number) ? 'yes' : 'no';
-  return [question, correctAnswer];
-};
-
-export { gameDescription, generateRound };
+  const number = getRandomNumber()
+  const question = number.toString()
+  const correctAnswer = isEven(number) ? 'yes' : 'no'
+  return [question, correctAnswer]
+}
+export { gameDescription, generateRound }
