344a728c967ded1d1a10a1efd9f6adff266db122
TimoEvt <t.evteev@school1387.ru>
2025-06-01 01:27:58 +0400

    brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
-import runGame from '../src/index.js';
-import generateRound from '../src/games/calc.js';
-
-const rule = 'What is the result of the expression?';
-
-runGame(generateRound, rule);
+import runGame from '../src/index.js'
+import generateRound from '../src/games/calc.js'
+const rule = 'What is the result of the expression?'
+runGame(generateRound, rule)
