0928ddac79c5d7ecd3edb50d52e8d0a13fbdae3e
TimoEvt <t.evteev@school1387.ru>
2025-06-02 15:32:51 +0400

    eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,27 @@
-import js from "@eslint/js"
-import globals from "globals"
-import { defineConfig } from "eslint/config"
+import js from '@eslint/js'
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs}"],
+    files: ['**/*.{js,mjs,cjs}'],
     extends: [js.configs.recommended],
     languageOptions: {
       globals: {
         ...globals.node,
         ...globals.browser,
-        require: "readonly",
-        module: "readonly",
-        console: "readonly",
-        process: "readonly",
+        require: 'readonly',
+        module: 'readonly',
+        console: 'readonly',
+        process: 'readonly',
       },
-      ecmaVersion: "latest",
-      sourceType: "module",
+      ecmaVersion: 'latest',
+      sourceType: 'module',
     },
     rules: {
       ...js.configs.recommended.rules,
-      "no-console": "off",
-      "import/extensions": "off",
+      "no-console": 'off',
+      "import/extensions": 'off',
     },
   },
 ])
