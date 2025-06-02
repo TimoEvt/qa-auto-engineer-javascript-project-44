ad7f9f3f6e0f91c1d981205981a43e33350579b0
TimoEvt <t.evteev@school1387.ru>
2025-06-02 15:07:20 +0400

    even.js

--- brain-games/even.js
+++ brain-games/even.js
@@ -1,18 +1,18 @@
-const runGameEngine = require('./engine');
+const runGameEngine = require('./engine')
 
 function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
 function generateEvenRound() {
-  const question = getRandomNumber(1, 100);
-  const correctAnswer = (question % 2 === 0) ? 'yes' : 'no';
-  return [question.toString(), correctAnswer];
+  const question = getRandomNumber(1, 100)
+  const correctAnswer = (question % 2 === 0) ? 'yes' : 'no'
+  return [question.toString(), correctAnswer]
 }
 
 function main() {
-  const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
-  runGameEngine(generateEvenRound, rule);
+  const rule = 'Answer "yes" if the number is even, otherwise answer "no".'
+  runGameEngine(generateEvenRound, rule)
 }
 
-main();
+main()
