cf017a81d5742bb3acf05796ddb38a880ae07ea2
TimoEvt <t.evteev@school1387.ru>
2025-06-02 15:24:14 +0400

    calc.js

--- brain-games/calc.js
+++ brain-games/calc.js
@@ -1,37 +1,37 @@
-const runGameEngine = require('./engine');
+const runGameEngine = require('./engine')
 
 function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
 function generateCalcRound() {
-  const operations = ['+', '-', '*'];
-  const num1 = getRandomNumber(1, 50);
-  const num2 = getRandomNumber(1, 50);
-  const operator = operations[Math.floor(Math.random() * operations.length)];
+  const operations = ['+', '-', '*']
+  const num1 = getRandomNumber(1, 50)
+  const num2 = getRandomNumber(1, 50)
+  const operator = operations[Math.floor(Math.random() * operations.length)]
 
-  let result;
+  let result
   switch (operator) {
     case '+':
       result = num1 + num2;
       break;
     case '-':
       result = num1 - num2;
-      break;
+      break
     case '*':
       result = num1 * num2;
-      break;
+      break
     default:
-      break;
+      break
   }
 
-  const question = `${num1} ${operator} ${num2}`;
-  return [question, result];
+  const question = `${num1} ${operator} ${num2}`
+  return [question, result]
 }
 
 function main() {
-  const rule = 'What is the result of the expression?';
-  runGameEngine(generateCalcRound, rule);
+  const rule = 'What is the result of the expression?'
+  runGameEngine(generateCalcRound, rule)
 }
 
-main();
+main()
