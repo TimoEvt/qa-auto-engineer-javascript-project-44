5df1c03800d9c5c5de64628ab94071e23c7b0055
TimoEvt <t.evteev@school1387.ru>
2025-06-09 22:10:31 +0400

    random.js

--- src/utils/random.js
+++ src/utils/random.js
@@ -1,2 +1,7 @@
-const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
-export default getRandomInt
+const getRandomInt = (min, max) => {
+  const minInt = Math.ceil(min);
+  const maxInt = Math.floor(max);
+  return Math.floor(Math.random() * (maxInt - minInt + 1)) + minInt;
+};
+
+export default getRandomInt;
