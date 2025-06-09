4de0909f9ffd45dba278fbea0ec6129c3b52fcb0
TimoEvt <t.evteev@school1387.ru>
2025-06-10 00:17:04 +0400

    calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -14,11 +14,11 @@ const generateRound = () => {
   let answer
   switch (operator) {
     case '+': answer = String(num1 + num2)
-    break
+      break
     case '-': answer = String(num1 - num2)
-    break
+      break
     case '*': answer = String(num1 * num2)
-    break
+      break
     default:
       throw new Error(`Unknown operator: ${operator}`)
   }
