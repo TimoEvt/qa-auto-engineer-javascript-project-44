adfe55d150854e99c0b4308d78317ec4df8f4eb6
TimoEvt <t.evteev@school1387.ru>
2025-06-10 00:14:47 +0400

    brain-calc.js

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -1,29 +1,32 @@
-import runEngine from '../index.js';
-import getRandomInt from '../utils.js';
+import runEngine from '../index.js'
+import getRandomInt from '../utils.js'
 
-const description = 'What is the result of the expression?';
+const description = 'What is the result of the expression?'
 
 const generateRound = () => {
-  const operators = ['+', '-', '*'];
-  const num1 = getRandomInt(1, 100);
-  const num2 = getRandomInt(1, 100);
-  const operator = operators[getRandomInt(0, operators.length - 1)];
+  const operators = ['+', '-', '*']
+  const num1 = getRandomInt(1, 100)
+  const num2 = getRandomInt(1, 100)
+  const operator = operators[getRandomInt(0, operators.length - 1)]
 
-  const question = `\${num1} \${operator} \${num2}`;
-  let answer;
+  const question = `\${num1} \${operator} \${num2}`
+  let answer
   switch (operator) {
-    case '+': answer = String(num1 + num2); break;
-    case '-': answer = String(num1 - num2); break;
-    case '*': answer = String(num1 * num2); break;
+    case '+': answer = String(num1 + num2)
+    break
+    case '-': answer = String(num1 - num2)
+    break
+    case '*': answer = String(num1 * num2)
+    break
     default:
-      throw new Error(`Unknown operator: \${operator}`);
+      throw new Error(`Unknown operator: \${operator}`)
   }
 
-  return [question, answer];
-};
+  return [question, answer]
+}
 
 const runCalcGame = () => {
-  runEngine(description, generateRound);
-};
+  runEngine(description, generateRound)
+}
 
-export default runCalcGame;
+export default runCalcGame
