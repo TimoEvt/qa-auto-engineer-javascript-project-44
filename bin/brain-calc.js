d7d5fa0f477a5e8a080b5f48de588dac5e89f925
Tim Evteev <timofey@Tims-MacBook-Air.local>
2025-06-09 21:42:24 +0400

    Refactor: move game logic into src/games/calc.js and slim down bin/brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-import runGame from '../src/index.js'
-import generateRound from '../src/games/calc.js'
-const rule = 'What is the result of the expression?'
-runGame(generateRound, rule)
+import runCalcGame from '../src/games/calc.js';
+
+runCalcGame();
--- src/games/calc.js
+++ src/games/calc.js
@@ -1,29 +1,29 @@
-import getRandomInt from '../utils/random.js'
+import runEngine from '../index.js';
+import getRandomInt from '../utils.js';
 
-const operators = ['+', '-', '*']
+const description = 'What is the result of the expression?';
 
-const getCalcRound = () => {
-  const num1 = getRandomInt(1, 50)
-  const num2 = getRandomInt(1, 50)
-  const operator = operators[getRandomInt(0, operators.length - 1)]
-  const question = `${num1} ${operator} ${num2}`
+const generateRound = () => {
+  const operators = ['+', '-', '*'];
+  const num1 = getRandomInt(1, 100);
+  const num2 = getRandomInt(1, 100);
+  const operator = operators[getRandomInt(0, operators.length - 1)];
 
-  let correctAnswer
+  const question = \`\${num1} \${operator} \${num2}\`;
+  let answer;
   switch (operator) {
-    case '+':
-      correctAnswer = num1 + num2
-      break
-    case '-':
-      correctAnswer = num1 - num2
-      break
-    case '*':
-      correctAnswer = num1 * num2
-      break
+    case '+': answer = String(num1 + num2); break;
+    case '-': answer = String(num1 - num2); break;
+    case '*': answer = String(num1 * num2); break;
     default:
-      throw new Error(`Unknown operator: ${operator}`)
+      throw new Error(\`Unknown operator: \${operator}\`);
   }
 
-  return [question, String(correctAnswer)]
-}
+  return [question, answer];
+};
 
-export default getCalcRound
+const runCalcGame = () => {
+  runEngine(description, generateRound);
+};
+
+export default runCalcGame;
