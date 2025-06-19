2a7748ab0a34430eb2287216870245a7ecc45c04
TimoEvt <t.evteev@school1387.ru>
2025-06-19 19:34:06 +0400

    solution.test.js

--- __tests__/solution.test.js
+++ __tests__/solution.test.js
@@ -1,1 +1,6 @@
-import solution from '../src/solution.js'; test('regex matches ruby1.js', () => { expect('ruby1.js').toMatch(solution); expect('ruby2.js').not.toMatch(solution); });
+import solution from '../src/solution.js'
+
+test('regex matches ruby1.js', () => {
+  expect('ruby1.js').toMatch(solution)
+  expect('ruby2.js').not.toMatch(solution)
+})
