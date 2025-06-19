b6cac154e97b1eb58bd8b7bb34b8ce61cea52801
TimoEvt <t.evteev@school1387.ru>
2025-06-19 19:18:21 +0400

    brain-calc.js

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -1,4 +1,4 @@
-import runEngine from '../index.js'
+import runGame from '../index.js'
 import getRandomInt from '../utils.js'
 
 const description = 'What is the result of the expression?'
@@ -9,7 +9,7 @@ const generateRound = () => {
   const num2 = getRandomInt(1, 100)
   const operator = operators[getRandomInt(0, operators.length - 1)]
 
-  const question = `Question: ${num1} ${operator} ${num2}`
+  const question = `${num1} ${operator} ${num2}`
   let answer
   switch (operator) {
     case '+': answer = String(num1 + num2)
@@ -19,14 +19,14 @@ const generateRound = () => {
     case '*': answer = String(num1 * num2)
       break
     default:
-      throw new Error(`Unknown operator: \${operator}`)
+      throw new Error(`Unknown operator: ${operator}`)
   }
 
   return [question, answer]
 }
 
 const runCalcGame = () => {
-  runEngine(description, generateRound)
+  runGame(generateRound, description)
 }
 
 export default runCalcGame
