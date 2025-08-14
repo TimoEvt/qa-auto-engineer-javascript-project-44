cbd3db04948c4f4bcbe306467f6a0ed85e22345f
TimoEvt <t.evteev@school1387.ru>
2025-08-14 20:00:00 +0400

    eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,3 @@
-// eslint.config.js
-import js from '@eslint/js'
 import globals from 'globals'
 import jest from 'eslint-plugin-jest'
 
@@ -8,23 +6,23 @@ export default [
     env: {
       browser: true,
       node: true,
-      jest: true, // чтобы ESLint видел test/expect
+      jest: true
     },
     globals: {
       ...globals.browser,
-      ...globals.node,
+      ...globals.node
     },
     plugins: {
-      jest,
+      jest
     },
     rules: {
       semi: ['error', 'always'],
       quotes: ['error', 'single'],
-      "quote-props": ['error', 'as-needed'],
+      'quote-props': ['error', 'as-needed']
     },
     parserOptions: {
       ecmaVersion: 2024,
-      sourceType: 'module',
-    },
-  },
+      sourceType: 'module'
+    }
+  }
 ]
