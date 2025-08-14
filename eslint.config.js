050fa0fb55d8716904f543df7f44ae33028391bd
TimoEvt <t.evteev@school1387.ru>
2025-08-14 19:28:47 +0400

    eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,20 @@
 import js from '@eslint/js'
-import globals from 'globals'
 import jest from 'eslint-plugin-jest'
 
 export default [
   {
     files: ['**/*.{js,mjs,cjs}'],
-    plugins: { js, jest },
+    plugins: ['jest'],
     languageOptions: {
       env: {
         node: true,
         jest: true, // чтобы ESLint видел test/expect
       },
-      globals: {
-        ...globals.node,
-      },
     },
     rules: {
       ...js.configs.recommended.rules,
       ...jest.configs.recommended.rules,
-      semi: ['error', 'never'], 
+      semi: ['error', 'never'], // правильно: строка 'never'
     },
   },
 ]
-  
